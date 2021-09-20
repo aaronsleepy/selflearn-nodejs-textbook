318322404aa26ea425e8ba0985bdf7853f0558da
aaronsleepy <aaron@kmong.com>
2021-09-20 15:05:48 +0900

    task: add events test

---
+++ ch3/module/module-events/event.js
@@ -0,0 +1,42 @@
+// Custom event emit
+
+const EventEmitter = require('events');
+
+const myEvent = new EventEmitter();
+myEvent.addListener('event1', () => {
+    console.log('event #1');
+});
+
+myEvent.on('event2', () => {
+    console.log('event #2');
+});
+myEvent.on('event2', () => {
+    console.log('event #2 added');
+});
+
+myEvent.once('event3', () => {
+    console.log('event #3');
+});
+
+myEvent.emit('event1');
+myEvent.emit('event2');
+
+myEvent.emit('event3');
+myEvent.emit('event3');
+
+myEvent.on('event4', () => {
+    console.log('event #4');
+});
+
+myEvent.removeAllListeners('event4');
+myEvent.emit('event4');
+
+const listener = () => {
+    console.log('event #5');
+};
+
+myEvent.on('event5', listener);
+myEvent.removeListener('event5', listener);
+myEvent.emit('event5');
+
+console.log(myEvent.listenerCount('event2'))
